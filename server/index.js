034c0e043db5b4a756d70cf04d3707d781c558d7
asalmeron3 <arturosalmeron24@gmail.com>
2025-12-18 10:58:53 -0600

    updating db connection variables

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const cors = require('cors');
 
 const projectsRoutes = require('./routes/projects');
+const PORT = process.env.DB_PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/projects', projectsRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
